d24fbef902587cd2757f61d48dedaccd4e69b22b
Mikhail <egorenkov200@gmail.com>
2023-02-12 20:06:34 +0800

    gg

---
+++ homework/hw3.3/Program.cs
@@ -0,0 +1,46 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//Пример:
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int number = ReadInt("Введите число N");
+int i = 1;
+while(i <= number)
+{
+    Console.WriteLine($"{i*i*i} ");
+    i++;
+}
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ homework/hw3/Program.cs
@@ -0,0 +1,45 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//Пример:
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+int InputInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int reverseDigits(int num)
+{
+    int rev = 0;
+    while (num > 0)
+    {
+        rev = rev * 10 + num % 10;
+        num = num / 10;
+    }
+    return rev;
+}
+
+int number = InputInt("Введите число > ");
+int reverse = reverseDigits(number);
+if (number == reverse)
+{
+    System.Console.WriteLine("Полиндром");
+}
+else
+{
+    System.Console.WriteLine("Не полиндром");
+}
+
+
+
+
+
+
+
+
+
+
+
